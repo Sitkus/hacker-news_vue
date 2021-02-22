793af955852c4822c50cf3b490716764ae5faaf2
Sitkus <65811246+Sitkus@users.noreply.github.com>
2021-02-22 11:09:00 +0200

    Added factory functions to ItemList testing

--- src/views/__tests__/ItemList.spec.js
+++ src/views/__tests__/ItemList.spec.js
@@ -1,4 +1,5 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils';
+import mergeWith from 'lodash.mergewith';
 import Vuex from 'vuex';
 import flushPromises from 'flush-promises';
 import ItemList from '../ItemList.vue';
@@ -7,11 +8,18 @@ import { Item } from '@/components';
 const localVue = createLocalVue();
 localVue.use(Vuex);
 
-let storeOptions = null;
-let store = null;
+function customizer(objValue, srcValue) {
+  if (Array.isArray(srcValue)) {
+    return srcValue;
+  }
 
-beforeEach(() => {
-  storeOptions = {
+  if (srcValue instanceof Object && Object.keys(srcValue).length === 0) {
+    return srcValue;
+  }
+}
+
+function createStore(overrides) {
+  const defaultStoreConfig = {
     getters: {
       displayItems: jest.fn()
     },
@@ -20,26 +28,37 @@ beforeEach(() => {
     }
   };
 
-  store = new Vuex.Store(storeOptions);
-});
+  return new Vuex.Store(mergeWith(defaultStoreConfig, overrides, customizer));
+}
+
+function createWrapper(overrides) {
+  const defaultMountingOptions = {
+    mocks: {
+      $bar: {
+        start: jest.fn(),
+        finish: jest.fn(),
+        fail: jest.fn()
+      }
+    },
+    localVue,
+    store: createStore()
+  };
+
+  return shallowMount(ItemList, mergeWith(defaultMountingOptions, overrides, customizer));
+}
 
 describe('ItemList.vue', () => {
   test('renders an Item with data for each item in displayItems', () => {
-    const $bar = {
-      start: () => {},
-      finish: () => {}
-    };
-
     const items = [{}, {}, {}];
-    storeOptions.getters.displayItems.mockReturnValue(items);
-
-    const wrapper = shallowMount(ItemList, {
-      mocks: { $bar },
-      localVue,
-      store
+    const store = createStore({
+      getters: {
+        displayItems: () => items
+      }
     });
 
+    const wrapper = createWrapper({ store });
     const Items = wrapper.findAllComponents(Item);
+
     expect(Items).toHaveLength(items.length);
 
     Items.wrappers.forEach((wrapper, index) => {
@@ -48,75 +67,61 @@ describe('ItemList.vue', () => {
   });
 
   test('calls $bar start on load', () => {
-    const $bar = {
-      start: jest.fn(),
-      finish: () => {}
+    const mocks = {
+      $bar: {
+        start: jest.fn()
+      }
     };
 
-    shallowMount(ItemList, {
-      mocks: { $bar },
-      localVue,
-      store
-    });
+    createWrapper({ mocks });
 
-    expect($bar.start).toHaveBeenCalled();
+    expect(mocks.$bar.start).toHaveBeenCalled();
   });
 
   test('calls $bar.finish when load is successful', async () => {
     expect.assertions(1);
 
-    const $bar = {
-      start: jest.fn(),
-      finish: jest.fn()
+    const mocks = {
+      $bar: {
+        finish: jest.fn()
+      }
     };
 
-    shallowMount(ItemList, {
-      mocks: { $bar },
-      localVue,
-      store
-    });
+    createWrapper({ mocks });
 
     await flushPromises();
 
-    expect($bar.finish).toHaveBeenCalled();
+    expect(mocks.$bar.finish).toHaveBeenCalled();
   });
 
   test('calls $bar.fail when load unsuccessful', async () => {
     expect.assertions(1);
 
-    const $bar = {
-      start: jest.fn(),
-      fail: jest.fn()
+    const store = createStore({
+      actions: {
+        fetchItems: jest.fn(() => Promise.reject())
+      }
+    });
+    const mocks = {
+      $bar: {
+        fail: jest.fn()
+      }
     };
 
-    storeOptions.actions.fetchItems.mockRejectedValueOnce();
-
-    shallowMount(ItemList, {
-      mocks: { $bar },
-      localVue,
-      store
-    });
+    createWrapper({ mocks, store });
 
     await flushPromises();
 
-    expect($bar.fail).toHaveBeenCalled();
+    expect(mocks.$bar.fail).toHaveBeenCalled();
   });
 
   test('dispatches fetchItems with top', async () => {
     expect.assertions(1);
 
-    const $bar = {
-      start: () => {},
-      finish: () => {}
-    };
-
+    const store = createStore();
     store.dispatch = jest.fn(() => Promise.resolve());
 
-    shallowMount(ItemList, {
-      mocks: { $bar },
-      localVue,
-      store
-    });
+    createWrapper({ store });
 
     expect(store.dispatch).toHaveBeenCalledWith('fetchItems', {
       type: 'top'
